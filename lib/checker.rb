2eee8fd4ca918746e89b11ef541fd58951fcee97
Keita Tsukaguchi <keitatsukaguchi@KeitanoMacBook-Pro.local>
2021-03-05 17:35:47 +0900

    fix file

--- lib/checker.rb
+++ lib/checker.rb
@@ -1,24 +1,14 @@
-#  def fizzbuzz_checker(n)
-#     if n % 15 == 0
-#       'FizzBuzz'
-#     elsif n % 3 == 0
-#       'Fizz'
-#     elsif n % 5 == 0
-#       'Buzz'
-#     else
-#       n.to_s
-#     end
-#   end
-
-# puts  fizzbuzz_checker(1)
-# puts  fizzbuzz_checker(2)
-# puts  fizzbuzz_checker(3)
-# puts  fizzbuzz_checker(4)
-# puts  fizzbuzz_checker(5)
-# puts  fizzbuzz_checker(6)
-# puts  fizzbuzz_checker(7)
-# puts  fizzbuzz_checker(8)
-# puts  fizzbuzz_checker(1..10)
+ def fizzbuzz_checker(n)
+    if n % 15 == 0
+      'FizzBuzz'
+    elsif n % 3 == 0
+      'Fizz'
+    elsif n % 5 == 0
+      'Buzz'
+    else
+      n.to_s
+    end
+  end
 
 for i in 1..100
 
